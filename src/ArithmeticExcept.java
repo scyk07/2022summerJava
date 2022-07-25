7a89c1931294aacbf14ed3ce1370c1a09b58610a
scyk07 <scyk07@sen.go.kr>
2022-07-25 10:18:09 +0900

    src: try-catch-final

--- src/ArithmeticExcept.java
+++ src/ArithmeticExcept.java
@@ -1,9 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ArithmeticExcept {
     public static void main(String[] args) {
         try {
             System.out.println((10/0));
-        } catch (Exception e){
+            FileReader f = new FileReader("aa.txt");
+        } catch (ArithmeticException e){
+            System.out.println("!!ArithmeticException!!");
+        } catch (FileNotFoundException e){
+            System.out.println("!!No File!!");
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally{
+            System.out.println("Enter Final");
         }
         System.out.println("Normal Ends");
     }
